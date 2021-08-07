96d3678617085e8f03905071d97f7a4b7c42d6fa
Bartlomiej Telec <bartlomiej.andrzej.telec@hurtigruten.com>
2021-08-07 15:35:31 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CollectNGo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
